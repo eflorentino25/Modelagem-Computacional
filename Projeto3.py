d4136025c665b3be2d40fea982199e2cb9f44a6c
RafaVasc01 <50430484+RafaVasc01@users.noreply.github.com>
2019-05-14 01:47:59 -0300

    Update Projeto3.py

--- Projeto3.py
+++ Projeto3.py
@@ -6,5 +6,19 @@
 
 import matplotlib
 import os
+import numpy
+import matplotlib.pyplot as plt
+import pandas as pd
+from random import *
+
+x=[]
+y=[]
+for i in range (0,10,1):
+    value=random()
+    x.append(value)
+    #print(x)
+    value=random()
+    y.append(value)
+    #print(y)
 
 
