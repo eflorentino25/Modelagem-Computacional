b1e18412cb23b46d11f024de13c9a076d2501a65
RafaVasc01 <50430484+RafaVasc01@users.noreply.github.com>
2019-05-15 01:56:19 -0300

    Update Projeto3.py
    Para testar o programa, estou utilizando 6 nós e utilizando a matriz de adjacência A. 
    
    Falta adicionar os valores de x e y de cada nó (gerados a cada iteração) em um array especifico (no caso, dois array's por nó). Com esses array's podemos plotar os gráficos. 
    
    Na linha 32 a 34 (comentado), gerei o grafo barabasi e a função para obter a matriz de adjacencia deste. Com o algoritmo pronto, excluímos a matriz A de teste e configuramos em n (linha 15) a quantidade de nós.

--- Projeto3.py
+++ Projeto3.py
@@ -4,21 +4,64 @@
 ##############################################################################
 
 
-import matplotlib
-import os
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from random import *
 
-x=[]
-y=[]
-for i in range (0,10,1):
+np.set_printoptions(threshold=sys.maxsize) #visualise matrix without truncation
+#numpy.set_printoptions(threshold=2) #visualise matrix with truncation
+
+n=6# number of nodes
+
+x=[]# empty matrix of x
+y=[]# empty matrix of y
+for i in range (0,n+1,1): # generate random initial values for x and y 
     value=random()
     x.append(value)
     #print(x)
     value=random()
     y.append(value)
     #print(y)
+    
+
+
+A= np.matrix([[0,1,1,0,0,0],[1,0,0,1,0,0],[1,0,0,0,1,0],[0,1,0,0,1,0],[0,0,1,1,0,1],[0,0,0,0,1,0]])# test adjacency matrix
+
+
+#ba = nx.barabasi_albert_graph(500, 2)#generate barabasi_albert matrix with 500 nodes and 2 edges
+#A = nx.to_numpy_matrix(ba)# generate adjacency matrix 
+#print(A)
+
+
+S=np.zeros(n) # array for values of coupling
+Si=[] # stores values of S
+dt=1 #time increment
+
+w=0.1 # coupling force
+theta=0.5 # cutting threshold
+I=0.2 # external stimulus
+p=0 # noise signal
+e=0.02 # model parameter epsilon
+a=6 # model parameter alpha
+b=0.1 # model parameter beta
+
+for t in range (0,1,1):# iteration begin (0,end time Tmáx,increment=1)
+    for i in range (0,n,1):# calculation of coupling influence, changing i index of A[i][j] 
+        S[i]=0
+        for j in range (0,6,1):# calculation of coupling influence, changing j index of A[i][j] 
+            if A[i,j]==1 and x[j]>theta:
+                S[i]=S[i]+w
+        Si.append(S[i]) # attach S[i] to Si
+    
+    dx=np.zeros(n)
+    dy=np.zeros(n)
+    for k in range (0,n,1):# integration of neurons with Euler method
+            dx[k]=(3*x[k]-(x[k]*x[k])-y[k]+I+p+S[k])*dt
+            dy[k]=(e*(a*(1+np.tanh(x[k]/b))-y[k]))*dt
+            x[k+1]=x[k]+dx[k]
+            y[k+1]=y[k]+dy[k]
+            print("x: ",x[k])
+            print("y: ",y[k])
 
 
